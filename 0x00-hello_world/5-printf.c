575f881ec5b6bc1f26a5d5e993d4eb639f4b1e5b
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-18 10:55:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdoi.h>
+
+/**
+ * main - entery point 
+ * Return: 0
+ * Descripyion: output a string 
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
