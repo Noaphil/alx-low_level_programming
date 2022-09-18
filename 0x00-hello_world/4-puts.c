139fb4bd143b1e09b12c9fecaaa40692d37d20ef
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-18 10:29:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - entry point
- * Return : 0
+ * Return: 0
  * Description: output a string
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
