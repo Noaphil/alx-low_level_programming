501cc64853d4f3ea42a4408fe7e5629e06026f68
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-22 12:58:40 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for lower case character
+ * @c: parameters
+ * Return: 0 (success) and 1 (otherwise)0
+ */
+
+int _isdigit(int c)
+{
+	return  (c >= '0' && c <= '9');
+}
