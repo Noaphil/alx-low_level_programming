fd81d8a7706e3a4e4bd2fd5560aa31069eae8486
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-22 13:02:35 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *
+ * mul - checks for lower case character
+ * @a: parameter a
+ * @b: parameter b
+ * Return: a multipied by b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
