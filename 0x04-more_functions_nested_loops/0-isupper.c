26ccb9f839d507443a5cdacba4ea81908e2787d6
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-22 12:53:38 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for lower case character
+ * @c: parameters
+ * Return: 0 (success) and 1 (otherwise)0
+ */
+
+int _isupper(int c)
+{
+
+	return  (c >= 65 && c <= 90);
+}
