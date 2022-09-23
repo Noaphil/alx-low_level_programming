5bcdef68ebc07fa904d46b78a57967faa197f295
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-22 18:23:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	int digit = 0;
+	
+	while (digit < 10)
+	{
+		putchar(digit + 48);
+		digit++;
+	}
+	while (alphabet <= 'f')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
