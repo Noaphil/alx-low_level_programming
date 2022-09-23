598b296eaafa086857457ac287939e566ea016c8
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-22 18:15:16 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit + 48);
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
