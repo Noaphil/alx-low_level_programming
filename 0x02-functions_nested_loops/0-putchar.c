ee636e5e3e059b18b0995332ad136670a12ef942
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-09-23 13:03:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**main - Program entry point
+ * Description: program prints _putchar
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char *string = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(string[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
