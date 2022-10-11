78a57f905da563b29d062a48c20ab150dd05b49e
nnamchi anthonia <nnamchianthonia1995@gmail.com>
2022-10-10 22:34:43 -0700

    5-sign.c

--- 0x0E-structures_typedef/5-sign.c
+++ 0x0E-structures_typedef/5-sign.c
@@ -1,24 +1,13 @@
-#include "main.h"
-/**
- * print_sign - function to check for a sign of a number
- * @n:  is the int that will use for the argument of the function
- * Return: 0
- */
-int print_sign(int n)
+#include <stdlib.h> include "dog.h"
+
+/** * free_dog - free memory *@d: array * Description: Free memory for struct */
+
+void free_dog(dog_t *d)
 {
-	if (n > 0)
+	if (d)
 	{
-	_putchar('+');
-	return (1);
-	}
-	else if (n < 0)
-	{
-	_putchar('-');
-	return (-1);
-	}
-	else
-	{
-	_putchar('0');
-	return (0);
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }
